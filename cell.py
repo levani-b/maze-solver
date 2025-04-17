48d26bb45a1a0d96267275ffdf91f5b952c310a9
levani-b <levanbirchadze@gmail.com>
2025-04-17 16:55:10 +0400

    update cell and implemented maze class with cell creation and drawing functionality

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 from graphics import Line, Point
 
+
 class Cell:
     def __init__(self, win):
         self.has_left_wall = True
@@ -12,7 +13,6 @@ class Cell:
         self._y2 = None
         self._win = win
 
-    
     def draw(self, x1, y1, x2, y2):
         if self._win is None:
             return
@@ -21,18 +21,18 @@ class Cell:
         self._y1 = y1
         self._y2 = y2
         if self.has_left_wall:
-            line = Line(Point(self._x1, self._y1),Point(self._x2, self._y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
         if self.has_top_wall:
-            line = Line(Point(self._x1, self._y1),Point(self._x2, self._y2))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
         if self.has_right_wall:
-            line = Line(Point(self._x1, self._y1),Point(self._x2, self._y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
         if self.has_bottom_wall:
-            line = Line(Point(self._x1, self._y1),Point(self._x2, self._y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
-    
+
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self._x2 - self._x1) // 2
         x_center = half_length + self._x1
